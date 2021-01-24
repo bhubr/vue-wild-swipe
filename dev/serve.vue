b86413bc4ff2c115745290a910103d35f489e683
Benoît Hubert <benoithubert@gmail.com>
2021-01-25 00:44:07 +0100

    Init module

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import VueSimpleSwipe from '@/vue-simple-swipe.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    VueSimpleSwipe
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue-simple-swipe />
+  </div>
+</template>
